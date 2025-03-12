d49fea4af6193221129600144fdf0a92f3c5e715
Valentin Brandl <valentin.brandl@ebsnet.de>
2025-03-12 13:00:01 +0100

    Get version from build parameters

---
+++ src/main/java/de/ebsnet/keygen/VersionProvider.java
@@ -0,0 +1,32 @@
+package de.ebsnet.keygen;
+
+import java.io.IOException;
+import java.util.Properties;
+import picocli.CommandLine;
+
+public final class VersionProvider implements CommandLine.IVersionProvider {
+  @Override
+  public String[] getVersion() {
+    return new String[] {ValueHolder.VERSION};
+  }
+
+  private static final class ValueHolder {
+    private static final String VERSION;
+
+    static {
+      String version;
+      String commit;
+      try (var stream =
+          VersionProvider.class.getClassLoader().getResourceAsStream("version.properties")) {
+        final var prop = new Properties();
+        prop.load(stream);
+        version = prop.getProperty("version", "0.0.0");
+        commit = prop.getProperty("commit", "unknown commit");
+      } catch (final IOException ex) {
+        version = "-0.0.0";
+        commit = "unknown commit";
+      }
+      VERSION = version + " (" + commit + ") https://www.ebsnet.de";
+    }
+  }
+}
